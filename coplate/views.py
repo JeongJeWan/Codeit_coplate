0611074303c39d757869be1b513d9a585463f28b
jewan <jewan@codeit.com>
2022-04-27 09:43:20 +0900

    django Review revise page Create

--- coplate/views.py
+++ coplate/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+)
 from allauth.account.views import PasswordChangeView
 from coplate.models import Review
 from coplate.forms import ReviewForm
@@ -24,6 +29,23 @@ class ReviewCreataView(CreateView):
     model = Review
     form_class = ReviewForm
     template_name = "coplate/review_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("review-detail", kwargs={"review_id": self.object.id})
+
+
+class ReviewUpdateView(UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "coplate/review_form.html"
+    pk_url_kwarg = "review_id"
+
+    def get_success_url(self):
+        return reverse("review-detail", kwargs={"review_id": self.object.id})
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
